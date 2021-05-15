71d1bfee94d68a763861ca1ac69e9e3ae19eeadf
Fri13nds <fri13nds@pop-os.localdomain>
2021-05-15 10:38:57 -0400

    add heroku server

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,6 @@ app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.resolve(__dirname, "..", "temp", "uploads")))
 
-app.listen(3333, () =>{
+app.listen(process.env.PORT || 3333, () =>{
   console.log('Started Server in Port 3333!')
 })
